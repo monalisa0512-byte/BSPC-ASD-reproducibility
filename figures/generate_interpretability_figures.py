920b43b90c6fdf7cf9fda6597c4378fa295a4434
ZiZheng Liu <monalisa0512@163.com>
2026-07-27 20:44:58 +0800

    Use Times New Roman in generate_interpretability_figures.py

--- figures/generate_interpretability_figures.py
+++ figures/generate_interpretability_figures.py
@@ -10,9 +10,18 @@ import os
 import glob
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy import stats
 from common_paths import DATA_DIR as DATA_PATH, RESULTS_DIR, OUTPUT_DIR as FIGURE_OUTPUT_DIR
+
+mpl.rcParams.update({
+    'font.family': 'serif',
+    'font.serif': ['Times New Roman', 'Times', 'DejaVu Serif'],
+    'mathtext.fontset': 'stix',
+    'pdf.fonttype': 42,
+    'svg.fonttype': 'none',
+})
 
 # Optional: t-SNE
 try:
